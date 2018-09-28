523fd74c207a3f0b909acaf9b050b9808c603a15
JC Chang <jerryjcchang@gmail.com>
2018-09-28 22:59:28 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,10 +4,10 @@ def position_taken?(board, x)
 if board[x] == " " || ""
   return false
 
- else board[x] == nil 
+  else board[x] == nil 
   return false
   
-   elsif board[x] == "X" || "O"
+  else board[x] == "X" || "O"
   return true
 end
 end
